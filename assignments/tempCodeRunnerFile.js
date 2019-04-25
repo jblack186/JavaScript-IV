8d671ea06efa5abae3dcd72689ee53174483169b
Jamison Blackwell <blackwellj1040@gmail.com>
2019-04-25 19:28:55 -0400

    finsihed JavaScropt IV project

--- assignments/tempCodeRunnerFile.js
+++ assignments/tempCodeRunnerFile.js
@@ -1,1 +1,11 @@
-steveSlack
+ function Hero(heroAttrs) {
+//     Humanoid.call(this, heroAttrs);
+   
+//   }
+//   Hero.prototype = Object.create(Humanoid.prototype);
+  
+//   function Villian(villAttrs) {
+//     Humanoid.call(this, villAttrs);
+   
+//   }
+//   Villian.prototype = Object.create(Hero.prototype);
