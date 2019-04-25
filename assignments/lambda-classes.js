4b418987049b754f9f00d5ff61cc6c1d5a47291e
Jamison Blackwell <blackwellj1040@gmail.com>
2019-04-24 21:40:31 -0400

    started lambda-classes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,22 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender;
+    }
+    speak() {
+        `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(name) {
+        super(name);
+        this.specialty = 'redux';
+        this.favLanguage = ['JavaScript', 'Python', 'Elm'];
+        this.catchPhrase = `Don't forget the homies`;
+    }
+    
+}
